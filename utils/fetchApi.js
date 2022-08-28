0145886bfdc90ecf8188873e8d05864619aed692
Oussama Sadiki <63022311+sadiki-o@users.noreply.github.com>
2022-08-28 16:37:34 +0100

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -7,7 +7,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com"
 export const fetchApi = async (url) => {
     const {data} = await axios.get(url, {
         headers: {
-            'x-RapidAPI-Key': '3f9134396fmsh9dabd38b59c9144p119fcajsn7a13d84bb9ff',
+            'x-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
             'x-RapidAPI-Host': 'bayut.p.rapidapi.com'
           }
     })
